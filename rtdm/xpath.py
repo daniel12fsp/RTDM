b98b6b10a22a71545efab552a1bd29e226ca996f
Daniel Fernandes <daniel12fsp@yahoo.com.br>
2014-04-10 11:00:17 -0400

    impressao da frequencia dos xpath

--- rtdm/xpath.py
+++ rtdm/xpath.py
@@ -16,29 +16,36 @@ def create(file_regex, file_xpath):
 			xpath = tree.getpath(elem.getparent()) + "//*"
 			xpath = re.sub("\[\d+\]","",xpath)
 			if(xpaths.__contains__(xpath) == False):
-				file_xpath.write(xpath+"\n")
-			xpaths[xpath] = None
+				xpaths[xpath] = 0
+			xpaths[xpath] = xpaths[xpath] + 1
+			
+
 
 	_create_single(file_regex, "ponto")
 	_create_single(file_regex, "interrogacao")
 	_create_single(file_regex, "asterisco")
 	_create_single(file_regex, "mais")
 
+
+	for key in xpaths:
+			print(key, xpaths[key])
+#			file_xpath.write(xpath+"\n")
+
 	file_xpath.close()
 	file_regex.close()
 
 def extraction(file_xpath, page_target, file_data):
-	file_xpath = open(file_xpath)
-	page_target = open(page_target)
-	tree = lxml_parser(page_target)
-	file_data = open(file_data,"w")
-	for xpath in file_xpath.readlines():
-		tags = tree.xpath(xpath[:-1])
-		if(tags != []):
-			for tag in tags:
-				if(tag.text and re.search("\w",tag.text)):
-					file_data.write(str((tag.tag,tag.text))+"\n")
 	try:
+		file_xpath = open(file_xpath)
+		page_target = open(page_target)
+		tree = lxml_parser(page_target)
+		file_data = open(file_data,"w")
+		for xpath in file_xpath.readlines():
+			tags = tree.xpath(xpath[:-1])
+			if(tags != []):
+				for tag in tags:
+					if(tag.text and re.search("\w",tag.text)):
+						file_data.write(str((tag.tag,tag.text))+"\n")
 
 		file_xpath.close()
 		page_target.close()
