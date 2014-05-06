4e6bd3da33ccc48aa44ed76eafb3ab10200ec237
Daniel Fernandes <daniel12fsp@yahoo.com.br>
2014-05-05 21:40:10 -0400

    Wrapper ponto frio - ok

--- wrapper_manual/ponto_frio/wrapper_ponto-frio.py
+++ wrapper_manual/ponto_frio/wrapper_ponto-frio.py
@@ -6,20 +6,21 @@ import json
 import os
 import glob
 import html
+import sys
 #<div id="descricao" class="descricao">(.|\n)*</div>
 def wrapper(filename):
 	page =  open(filename, "r").read()
 	result = re.search('<div id="descricao" class="descricao">(.*?)<div id="footer">', page, re.S)
-	#ficha =  bs4.BeautifulSoup(result.groups()).section.prettify()
 	ficha = result.group(0)
 	ficha = unidecode(html.unescape(ficha)).lower()
-	ficha = re.sub("\s{2,}","", ficha)
-	dados = re.findall("<dt>(.*?)</dt>.*?<dd>(.*?)</dd>", ficha, re.S)
+	ficha = re.sub("\s{2,}|\n","", ficha)
+	dados = re.findall("<dt.*?>(.*?)</dt>.*?<dd.*?>(.*?)</dd>", ficha, re.S)
 	return dados
 
 def remove_space(data):
 	data = re.sub("^ ","",data)
 	data = re.sub(" $","",data)
+	data = re.sub(r"<.*?>","", data)
 	return data
 
 def out_json(filename):
@@ -35,7 +36,7 @@ def out_json(filename):
 
 folder = '/media/doc/home/doc/2013/academico/project/Implementacao/paginas_html/ColetaUFAM/ColetaNova/wrappers/pontofrio_notebooks/'
 
-jsao = open(folder + "json_wrapper.json","w")
+jsao = open("/media/doc/home/doc/2013/academico/project/Implementacao/rtdm-git/estatistica/ponto-frio_notebooks/" + "json_wrapper.json","w")
 files = sorted(glob.glob(folder+'*.html'))
 
 for filename in files:
