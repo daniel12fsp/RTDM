86bedf20b7a55aeea910f23e434c6eedc98a8943
Daniel Fernandes <daniel12fsp@yahoo.com.br>
2014-05-22 22:38:58 -0400

    Nova modificação Continuação - 100% - add modulo de testagem

---
+++ rtdm/main_teste.py
@@ -0,0 +1,19 @@
+import main_rtdm as main
+import re
+import os
+from distutils.dir_util import mkpath as mkpath
+file_folders = "/media/doc/home/doc/2013/academico/project/Implementacao/rtdm-git/links_rtdm.txt"
+folders = open(file_folders, "r").readlines()
+i = 0
+for folder in folders:
+	print(folder)
+	i += 1
+	s = re.search("(.*)/(.*?)/$",folder)
+	path_pai, loja = s.groups()
+	path_test = path_pai + "/teste/" + loja + "/"
+	path_log = path_test + "/log/"
+	path_pages = path_pai + "/"+loja +"/"
+	mkpath(path_test)
+	mkpath(path_log)
+	main.prepare_vars(path_pages, path_test, path_log)
+	main.exec_rtdm()
