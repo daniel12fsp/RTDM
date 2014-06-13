585dd89c797ee1f65d286d3042f454945611cfec
Ervili Tarsila <ervili@ervili-Infoway-w7730.(none)>
2014-06-12 23:42:32 -0400

    Add dados estatisticos como Tam de T1 e T2 e a Porcentagem Relativa

--- wrapper_manual/americanas/select_notebook_page.py
+++ wrapper_manual/americanas/select_notebook_page.py
@@ -7,12 +7,14 @@ import glob
 def isValidationPage(page):
 	html = open(page,"r").read()
 	result = re.findall("""<dl>(.+?)</dl>""", html, re.S)
+	#result = re.search("""<title>Notebook(.*?)</title>""", html, re.S)
+	#print(result)
 	return result
-folder = '/home/ervili/Neemu/ColetaUFAM/ColetaNova/notebooks/americanas_notebook/'
-newFolder = '/home/ervili/Neemu/ColetaUFAM/ColetaNova/notebooks/americanas_notebook/notebooks/'
+folder = '/home/ervili/rtdm/teste/americanas_notebooks/notebooks  - Pages/'
+newFolder = '/home/ervili/rtdm/teste/americanas_notebooks/notebooks/'
 os.listdir(folder)
 for filename in glob.glob(folder+'*.html'):
-	print("PAGE: " + filename)
+	print("\nPAGE: " + filename)
 	if(isValidationPage(filename)):
 		print(filename + " movido")
 		shutil.copy2(filename,newFolder)
