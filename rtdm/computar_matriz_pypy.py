910696362c4fdc4cae38d847ce4393a9df43c5d4
Daniel Fernandes <daniel12fsp@yahoo.com.br>
2014-07-26 16:42:37 -0400

    Modificação para funcionamento em pastas distintas
    Em computar_matriz_pypy.py, tem que quando chamar usar o diretorio como argumento.
    	Assim: pypy computar_matriz_pypy.py path-diretorio

--- rtdm/computar_matriz_pypy.py
+++ rtdm/computar_matriz_pypy.py
@@ -5,6 +5,7 @@ import rtdm
 import sys
 from itertools import product
 from time import gmtime, strftime
+from joblib import Parallel, delayed
 
 """
 
@@ -24,30 +25,23 @@ def calc_rtdm(i, j, pages):
 	return i, j, op
 
 
-path = "/media/doc/home/doc/2013/academico/project/Implementacao/paginas_html/ColetaUFAM/ColetaNova/wrappers_sites/algumas_paginas_simples/"
-#path = "/home/azureuser/algumas_paginas_simples/"
-
+path = sys.argv[1]
+print(path)
 
 pages = file.list_sorted_pages(path)
 indice = range(len(pages))
 matrix = np.zeros(shape=(len(pages),len(pages)), dtype=np.uint)
-#par_pags = filter(lambda x: x[0] < x[1]  ,product(pages, pages)) # [(pag1,pag2)...]
-par_pags = product(indice, indice) # [(pag1,pag2)...]
-"""
+par_pags = filter(lambda x: x[0] < x[1]  ,product(indice, indice)) # [(pag1,pag2)...]
+#par_pags = product(indice, indice) # [(pag1,pag2)...]
 
-with concurrent.futures.ThreadPoolExecutor(max_workers=8) as executor:
-    future_result = [ executor.submit(calc_rtdm, i, j, pages) for (i, j) in par_pags]
-"""
 name = strftime("%Y-%m-%d %H:%M:%S", gmtime())
 print(name)
 
-from joblib import Parallel, delayed
 resultados = Parallel(n_jobs=-1, backend="multiprocessing")(delayed(calc_rtdm)(i, j, pages) for (i, j) in par_pags)
 
 for r in resultados:
 	i, j, op = r
 	matrix[i][j] = op
-
 
 
 name = strftime("%Y-%m-%d %H:%M:%S", gmtime())
